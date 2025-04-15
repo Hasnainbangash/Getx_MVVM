0a4b9f3ba374213d7bb39368aa26f14e76fc676d
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-15 17:25:42 +0500

    Aded the post api code and the print some data in debug mode only

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:getx_mvvm/data/app_exceptions.dart';
 import 'package:getx_mvvm/data/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
@@ -9,10 +10,39 @@ class NetworkApiServices extends BaseApiServices {
 
   @override
   Future getApi(String url) async {
+
+    if(kDebugMode) {
+      print(url);
+    }
+
     dynamic responseJson;
 
     try {
       final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw InternetException('');
+    } on TimeoutException {
+      throw RequestTimeOut('');
+    }
+
+    return responseJson;
+  }
+
+  @override
+  Future postApi(var data, String url) async {
+
+    if(kDebugMode) {
+      print(url);
+      print(data);
+    }
+
+    dynamic responseJson;
+
+    try {
+      final response = await http.post(Uri.parse(url),
+        body: jsonEncode(data),
+      ).timeout(const Duration(seconds: 10));
       responseJson = returnResponse(response);
     } on SocketException {
       throw InternetException('');
