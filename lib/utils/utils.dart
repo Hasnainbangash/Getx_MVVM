0c5939012348c40d1527e130239f2505a35c5916
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-16 14:13:54 +0500

    Added a ssnackbar function in the utosl and added a toast message simple one in bototm gravity and one for center

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:getx_mvvm/res/colors/app_color.dart';
+import 'package:get/get.dart';
 
 class Utils {
 
@@ -16,9 +17,23 @@ class Utils {
     Fluttertoast.showToast(
         msg: message,
       backgroundColor: AppColor.blackColor,
+      gravity: ToastGravity.BOTTOM,
     );
   }
 
+  static toastMessageCenter(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: AppColor.blackColor,
+      gravity: ToastGravity.CENTER,
+    );
+  }
 
+  static snackBar(String message) {
+    Get.snackbar(
+        'title',
+        'message',
+    );
+  }
 
 }
