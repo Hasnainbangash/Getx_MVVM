5a42623abfc47c20702f0e6e2ab050b2066dc83d
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-16 14:34:51 +0500

    Another app color white is added along with in the splash screen use the toas messages to hceck the functionality

--- lib/res/colors/app_color.dart
+++ lib/res/colors/app_color.dart
@@ -2,4 +2,5 @@ import 'dart:ui';
 
 class AppColor {
   static const Color blackColor = Color(0xFF000000);
+  static const Color whiteColor = Color(0xFFFFFFFF);
 }
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getx_mvvm/data/app_exceptions.dart';
+import 'package:getx_mvvm/utils/utils.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,9 +13,15 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        throw InternetException('message');
-      }),
+      // floatingActionButton: FloatingActionButton(onPressed: (){
+      //   throw InternetException('message');
+      // }),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            Utils.toastMessageCenter('Hasnain');
+            Utils.toastMessage('Hasnain');
+          },
+      ),
     );
   }
 }
