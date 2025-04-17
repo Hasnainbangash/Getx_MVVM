a6d67d218635d4d39640ecc279bd2cdec6f06dbf
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-17 16:02:38 +0500

    Some basic colors are added to the project

---
+++ lib/res/components/internet_exception_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:getx_mvvm/res/colors/app_color.dart';
+
+class InternetExceptionWidget extends StatefulWidget {
+  const InternetExceptionWidget({super.key});
+
+  @override
+  State<InternetExceptionWidget> createState() => _InternetExceptionWidgetState();
+}
+
+class _InternetExceptionWidgetState extends State<InternetExceptionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Icon(Icons.cloud_off, color: AppColor.,),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getx_mvvm/data/app_exceptions.dart';
 import 'package:getx_mvvm/res/assets/image_assets.dart';
+import 'package:getx_mvvm/res/components/internet_exception_widget.dart';
 import 'package:getx_mvvm/utils/utils.dart';
 import 'package:get/get.dart';
 
@@ -14,22 +15,23 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('email_hint'.tr),
-      ),
-      // floatingActionButton: FloatingActionButton(onPressed: (){
-      //   throw InternetException('message');
-      // }),
-      body: Image(
-        image: AssetImage(ImageAssets.splashScreen),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Utils.toastMessageCenter('Hasnain');
-            Utils.toastMessage('Hasnain');
-          },
-      ),
-    );
+    return InternetExceptionWidget();
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('email_hint'.tr),
+    //   ),
+    //   // floatingActionButton: FloatingActionButton(onPressed: (){
+    //   //   throw InternetException('message');
+    //   // }),
+    //   body: Image(
+    //     image: AssetImage(ImageAssets.splashScreen),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //       onPressed: (){
+    //         Utils.toastMessageCenter('Hasnain');
+    //         Utils.toastMessage('Hasnain');
+    //       },
+    //   ),
+    // );
   }
 }
