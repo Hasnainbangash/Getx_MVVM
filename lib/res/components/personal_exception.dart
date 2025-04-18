6e1059ccdb7c3624d1c1e431228fe62053922b1c
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-18 15:05:11 +0500

    NEw file is added personal exception

---
+++ lib/res/components/personal_exception.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:getx_mvvm/res/colors/app_color.dart';
+import 'package:get/get.dart';
+
+class PersonalExceptionWidget extends StatefulWidget {
+
+  final VoidCallback onPress;
+
+  const PersonalExceptionWidget({super.key, required this.onPress});
+
+  @override
+  State<PersonalExceptionWidget> createState() => _PersonalExceptionWidgetState();
+}
+
+class _PersonalExceptionWidgetState extends State<PersonalExceptionWidget> {
+  @override
+  Widget build(BuildContext context) {
+
+    final height = MediaQuery.of(context).size.height;
+
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: height * 0.15),
+
+            Icon(Icons.cloud_off, color: AppColor.redColor, size: 50),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Text('internet_exception'.tr, textAlign: TextAlign.center,),
+            ),
+
+            SizedBox(height: height * 0.15),
+
+            InkWell(
+              onTap: widget.onPress,
+              child: Container(
+                height: 44,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: AppColor.primaryColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                    child: Text(
+                        'Retry',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
+                    ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
