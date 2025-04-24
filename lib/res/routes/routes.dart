fa1348df010e40e7e6820fbbfdd9950b17626696
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-24 18:24:58 +0500

    Home view nam eis added to the route namesa and aset it in the routes

--- lib/res/routes/routes.dart
+++ lib/res/routes/routes.dart
@@ -3,6 +3,8 @@ import 'package:getx_mvvm/res/routes/routes_name.dart';
 import 'package:getx_mvvm/view/login/login_view.dart';
 import 'package:getx_mvvm/view/splash_screen.dart';
 
+import '../../view/home/home_view.dart';
+
 class AppRoutes {
 
   static appRoutes() => [
@@ -15,6 +17,12 @@ class AppRoutes {
     GetPage(
       name: RoutesName.loginView,
       page: () => const LoginView(),
+      transitionDuration: Duration(milliseconds: 250),
+      transition: Transition.leftToRightWithFade,
+    ),
+    GetPage(
+      name: RoutesName.homeView,
+      page: () => const HomeView(),
       transitionDuration: Duration(milliseconds: 250),
       transition: Transition.leftToRightWithFade,
     ),
