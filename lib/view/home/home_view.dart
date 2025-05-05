467bc4d8f60b85b8f8a87cb41141a574e859a16f
Bangash38 <bangashmhasnain38@gmail.com>
2025-05-05 20:42:02 +0500

    in the home view screen a list view builder is made along with the data getting from the api

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_mvvm/res/routes/routes_name.dart';
+import 'package:getx_mvvm/view_models/controller/home/home_view_model.dart';
 import 'package:getx_mvvm/view_models/controller/user_preference/user_preference_view_model.dart';
+
+import '../../data/response/status.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,7 +15,16 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
 
+  final homeController = Get.put(HomeController());
   UserPreferences userPreferences = UserPreferences();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    homeController.userListApi();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +37,32 @@ class _HomeViewState extends State<HomeView> {
           }, icon: Icon(Icons.logout)),
         ],
       ),
+      body: Obx((){
+        switch(homeController.rxRequestStatus.value){
+          case Status.LOADING:
+            return Center(child: CircularProgressIndicator());
+          case Status.ERROR:
+            return Text('Something went wrong');
+          case Status.LOADING:
+            print('request successful');
+            return ListView.builder(
+              itemCount: homeController.userList.value.data!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(homeController.userList.value.data![index].avatar.toString()),
+                    ),
+                    title: Text(homeController.userList.value.data![index].firstName.toString()),
+                    subtitle: Text(homeController.userList.value.data![index].email.toString()),
+                  ),
+                );
+              },
+            );
+          default:
+        }
+        return SizedBox();
+      }),
     );
   }
 }
