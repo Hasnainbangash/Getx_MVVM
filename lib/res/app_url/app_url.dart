e58008e3908d6ce35a026f1d549174878142e100
Bangash38 <bangashmhasnain38@gmail.com>
2025-05-05 19:03:53 +0500

    Addeda new model fo the user list and added tyhe link of the get api in the appurls

--- lib/res/app_url/app_url.dart
+++ lib/res/app_url/app_url.dart
@@ -1,4 +1,5 @@
 class AppUrl {
   static const String baseUrl = 'https://reqres.in';
   static const String loginApi = '$baseUrl/api/login';
+  static const String userListApi = 'https://webhook.site/30e77ea3-d75a-4da4-9aa6-8376d06d3c8d';
 }
